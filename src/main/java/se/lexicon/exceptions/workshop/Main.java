d7cbc3df3873ac22200e53d67b7eb63c6b204659
Nalbi Stas <stas.nalbi@gmail.com>
2025-12-04 10:44:00 +0100

    Finished the assignment

--- src/main/java/se/lexicon/exceptions/workshop/Main.java
+++ src/main/java/se/lexicon/exceptions/workshop/Main.java
@@ -1,5 +1,6 @@
 package se.lexicon.exceptions.workshop;
 
+import java.io.IOException;
 import java.util.List;
 
 import se.lexicon.exceptions.workshop.data_access.NameService;
@@ -8,20 +9,29 @@ import se.lexicon.exceptions.workshop.fileIO.CSVReader_Writer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		List <String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
-        List <String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
+    public static void main(String[] args){
 
-        List <String> lastNames = CSVReader_Writer.getLastNames();
+        try{
+
+            List <String> maleFirstNames = CSVReader_Writer.getMaleFirstNames();
+            List <String> femaleFirstNames = CSVReader_Writer.getFemaleFirstNames();
+
+            List <String> lastNames = CSVReader_Writer.getLastNames();
 
 
-        NameService nameService = new NameService(maleFirstNames, femaleFirstNames,lastNames);
+
+            NameService nameService = new NameService(maleFirstNames, femaleFirstNames,lastNames);
 
 
-        Person test = nameService.getNewRandomPerson();
-        System.out.println(test);
+            Person test = nameService.getNewRandomPerson();
+            System.out.println(test);
 
-	}
+
+            nameService.addFemaleFirstName("Jorge");
+        }
+        catch(IOException e){
+            System.out.println("Error reading file");
+        }
+    }
 
 }
